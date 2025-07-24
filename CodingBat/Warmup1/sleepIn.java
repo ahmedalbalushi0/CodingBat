d1a5585f601e52962606d923417456133cabec79
Ahmed <ahmed1balushi3@gamil.com>
2025-07-24 15:54:03 +0400

    sleepIn done

---
+++ CodingBat/Warmup1/sleepIn.java
@@ -0,0 +1,9 @@
+package Warmup1;
+public class sleepIn {
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+        if(!weekday || vacation)
+            return true;
+        else
+            return false;
+    }
+}
