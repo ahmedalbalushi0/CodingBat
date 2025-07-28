66f495bfb14c356fdbe06752563d58877789c66b
Ahmed <ahmed1balushi3@gamil.com>
2025-07-28 23:47:05 +0400

    lastDigit is solved

---
+++ CodingBat/Warmup1/lastDigit.java
@@ -0,0 +1,10 @@
+package Warmup1;
+
+public class lastDigit {
+    public boolean lastDigit(int a, int b) {
+        if(a%10 == b%10)
+            return true;
+        else
+            return false;
+    }
+}
