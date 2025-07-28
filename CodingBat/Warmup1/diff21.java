b182317afaa2d2e52ee4f204b3200ced213909e3
Ahmed <ahmed1balushi3@gamil.com>
2025-07-28 13:18:24 +0400

    sovled diff21

---
+++ CodingBat/Warmup1/diff21.java
@@ -0,0 +1,10 @@
+package Warmup1;
+
+public class diff21 {
+    public int diff21(int n) {
+        if(n<21)
+            return Math.abs(n-21);
+        else
+            return Math.abs(n-21)*2;
+    }
+}
