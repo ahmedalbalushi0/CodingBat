d4398bb4eb847ccf9ebf92afc31b281ad94370a9
Ahmed <ahmed1balushi3@gamil.com>
2025-07-28 21:18:19 +0400

    or35 solved

---
+++ CodingBat/Warmup1/or35.java
@@ -0,0 +1,10 @@
+package Warmup1;
+
+public class or35 {
+    public boolean or35(int n) {
+        if(n>0 && n%3==0 || n%5==0)
+            return true;
+        else
+            return false;
+    }
+}
