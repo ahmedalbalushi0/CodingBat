1420e2248114fc325d294a98b4d7e77e6e76a2a8
Ahmed <ahmed1balushi3@gamil.com>
2025-07-30 00:23:33 +0400

    nearTen is solved

---
+++ CodingBat/Logic1/nearTen.java
@@ -0,0 +1,10 @@
+package Logic1;
+
+public class nearTen {
+    public boolean nearTen(int num) {
+        if (((num % 10)<=2 )||(num % 10)>=8)
+            return true;
+        else
+            return false;
+    }
+}
