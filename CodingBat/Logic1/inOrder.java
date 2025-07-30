3d41cb3536688b6e3653d5500da9dbbf8e86b11c
Ahmed <ahmed1balushi3@gamil.com>
2025-07-31 01:43:45 +0400

    inOrder is solved

---
+++ CodingBat/Logic1/inOrder.java
@@ -0,0 +1,13 @@
+package Logic1;
+
+public class inOrder {
+    public boolean inOrder(int a, int b, int c, boolean bOk) {
+        if(bOk && c>b)
+            return true;
+        else if(b>a && c>b){
+            return true;
+        }
+        else
+            return false;
+    }
+}
